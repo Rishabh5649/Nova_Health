6cea13f2d6f1bf635eb7222f4b7b04f1b6a8f791
Rishabh5649 <rvit23bis097.rvitm@rvei.edu.in>
2025-12-10 10:30:37 +0530

    Fix Calendar build error with next/dynamic ssr:false

--- hms_frontend_flutter/lib/core/env.dart
+++ hms_frontend_flutter/lib/core/env.dart
@@ -1,4 +1,4 @@
 class Env {
 // Emulator default. For real device, use your laptop LAN IP like 'http://192.168.1.5:3000'
-static const baseUrl = 'http://127.0.0.1:3000';
+  static const baseUrl = 'http://10.0.2.2:3000';
 }
